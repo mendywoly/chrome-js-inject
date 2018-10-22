60dd2673edb2173fcb64d54f605d0f1089c8864a
github username <github email address>
2018-10-22 17:51:52 -0400

    my updates

--- inject.js
+++ inject.js
@@ -1,15 +1,8 @@
 // this is the code which will be injected into a given page...
 
 (function() {
-
-	// just place a div at top right
-	var div = document.createElement('div');
-	div.style.position = 'fixed';
-	div.style.top = 0;
-	div.style.right = 0;
-	div.textContent = 'Injected!';
-	document.body.appendChild(div);
-
-	alert('inserted self... giggity');
-
+	const list = document.getElementsByClassName('drop-down')
+	const firstList = list[0]
+	firstList.children[2].selected = true
+	firstList.children[2].dispatchEvent(new Event('change', {bubbles: true}));
 })();
